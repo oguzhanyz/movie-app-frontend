78f4081de74b7dda0abfb84d5d6a35de47b01189
oguzhanyz <oguzhanyz74@gmail.com>
2024-12-25 17:33:52 +0300

    feat: add SelectForm component for genre filtering in Watchlist

---
+++ src/components/SelectForm.jsx
@@ -0,0 +1,44 @@
+import Select from "react-select";
+
+const options = [
+  { value: "action", label: "Action" },
+  { value: "adventure", label: "Adventure" },
+  { value: "animation", label: "Animation" },
+  { value: "biography", label: "Biography" },
+  { value: "comedy", label: "Comedy" },
+  { value: "crime", label: "Crime" },
+  { value: "documentary", label: "Documentary" },
+  { value: "drama", label: "Drama" },
+  { value: "family", label: "Family" },
+  { value: "fantasy", label: "Fantasy" },
+  { value: "film-noir", label: "Film-Noir" },
+  { value: "game-show", label: "Game-Show" },
+  { value: "history", label: "History" },
+  { value: "horror", label: "Horror" },
+  { value: "music", label: "Music" },
+  { value: "musical", label: "Musical" },
+  { value: "mystery", label: "Mystery" },
+  { value: "news", label: "News" },
+  { value: "reality-tv", label: "Reality-TV" },
+  { value: "romance", label: "Romance" },
+  { value: "sci-fi", label: "Sci-Fi" },
+  { value: "sport", label: "Sport" },
+  { value: "talk-show", label: "Talk-Show" },
+  { value: "thriller", label: "Thriller" },
+  { value: "war", label: "War" },
+  { value: "western", label: "Western" },
+];
+
+export default function SelectForm({ defaultValue, onChange }) {
+  return (
+    <Select
+      defaultValue={defaultValue}
+      onChange={onChange}
+      isMulti
+      name="genres"
+      options={options}
+      className="basic-multi-select"
+      classNamePrefix="select"
+    />
+  );
+}
--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -1,21 +1,74 @@
+import { useState } from "react";
 import MovieList from "../components/MovieList";
 import { useRetrieveWatchlist } from "../hooks/useRetrieveWatchlist";
 import useStore from "../hooks/useStore";
+import SelectForm from "../components/SelectForm";
 
 export default function Watchlist() {
   const watchlist = useStore((state) => state.watchlist);
   const { isLoading, error } = useRetrieveWatchlist();
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const itemPerPage = 3;
+
+  const filterArray = selectedGenres.map((obj) => obj.label);
+  const filteredWatchlist = watchlist.filter((movie) =>
+    movie.genres.some((genre) => filterArray.includes(genre)),
+  );
+
+  const maxPageNumber =
+    filteredWatchlist.length > 0
+      ? filteredWatchlist.length / itemPerPage
+      : watchlist.length / itemPerPage;
+
+  const paginatedWatchlist =
+    filteredWatchlist.length > 0
+      ? filteredWatchlist.slice(
+          (pageNumber - 1) * itemPerPage,
+          pageNumber * itemPerPage,
+        )
+      : watchlist.slice(
+          (pageNumber - 1) * itemPerPage,
+          pageNumber * itemPerPage,
+        );
+
+  function handleDecreasePageNumber() {
+    if (pageNumber > 1) setPageNumber((pN) => pN - 1);
+  }
+
+  function handleIncreasePageNumber() {
+    if (pageNumber < maxPageNumber) setPageNumber((pN) => pN + 1);
+  }
 
   return (
-    <>
+    <div className="flex flex-col items-center">
       {isLoading ? (
         <p>Loading...</p>
       ) : watchlist.length > 0 ? (
-        <MovieList movies={watchlist} />
+        <div>
+          <SelectForm
+            defaultValue={selectedGenres}
+            onChange={setSelectedGenres}
+          />
+          {filterArray.length > 0 ? (
+            filteredWatchlist.length > 0 ? (
+              <MovieList movies={paginatedWatchlist} />
+            ) : (
+              <p>No movies match the selected genres.</p>
+            )
+          ) : (
+            <MovieList movies={paginatedWatchlist} />
+          )}
+          <div className="flex justify-center gap-2 text-2xl">
+            <button onClick={handleDecreasePageNumber}> &lt; </button>
+            <span>{pageNumber}</span>
+            <button onClick={handleIncreasePageNumber}> &gt; </button>
+          </div>
+        </div>
       ) : (
         <p>Try searching a movie to add to your watchlist.</p>
       )}
       {error && <p style={{ color: "red" }}>{error}</p>}
-    </>
+    </div>
   );
 }
